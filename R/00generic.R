3e9e3d333b85d7110ad819cebe498e8b2c9e6451
Garnier Villarreal <m.garniervillarreal@vu.nl>
2025-08-27 13:32:00 +0200

    update generic show lca

--- R/00generic.R
+++ R/00generic.R
@@ -17,13 +17,22 @@ setMethod("show", "llca", function(object) {
   cat(sprintf("  %-45s %d\n\n", "Number of response patterns", object@modelInfo$npatterns))
 
   # Print Model Test Section
-  chisq <- -2*object@loglik
-  pval <- 1 - pchisq(chisq, df = object@modelInfo$df)
+  if(sum(model@modelInfo$item != "multinomial") == 0){
+    ni <- model@summary_table$Observed
+    mi <- model@summary_table$Estimated
+    df <- model@modelInfo$df
+    L2 <- 2*sum(ni*log(ni/mi))
+    pv <- 1-pchisq(L2, df)
+  }else{
+    L2 <- NA
+    pv <- NA
+    df <- NA
+  }
   cat("Model Test User Model:\n")
   cat("  ", paste(rep("-", 54), collapse = ""), "\n\n", sep = "")
-  cat(sprintf("  %-45s %.3f\n", "Test statistic", chisq))
-  cat(sprintf("  %-45s %d\n", "Degrees of freedom", object@modelInfo$df))
-  cat(sprintf("  %-45s %.3f\n", "P-value (Chi-square)", pval))
+  cat(sprintf("  %-45s %.3f\n", "Test statistic", L2))
+  cat(sprintf("  %-45s %d\n", "Degrees of freedom", df))
+  cat(sprintf("  %-45s %.3f\n", "P-value (L2)", pv))
 
   invisible(object)
 
